0878116f39721584a621050efd8e2413f207ed7c
Thom Williams <thomwilliams1990@gmail.com>
2021-05-04 07:53:47 +0100

    Test array tweak

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -5,28 +5,28 @@ describe("Engineer", () => {
   
     it("should constuct card data from engineer name, id and email input", () => {
       
-          const result = new Engineer ("Thom Williams", 7, "thomwilliams1990@gmail.com", 436347);
+      const result = new Engineer ("Thom Williams", 7, "thomwilliams1990@gmail.com", "GitHub Profile");
     
           expect(result.getRole()).toBe("Engineer");
     });
 
     it("get the email address of the Engineer", () => {
   
-        const result = new Engineer ("Thom Williams", 7, "thomwilliams1990@gmail.com", 436347);
+      const result = new Engineer ("Thom Williams", 7, "thomwilliams1990@gmail.com", "GitHub Profile");
   
       expect(result.getEmail()).toBe("thomwilliams1990@gmail.com");
     });
 
     it("get the name of the Engineer", () => {
   
-        const result = new Engineer ("Thom Williams", 7, "thomwilliams1990@gmail.com", 436347);
+      const result = new Engineer ("Thom Williams", 7, "thomwilliams1990@gmail.com", "GitHub Profile");
   
       expect(result.getName()).toBe("Thom Williams");
     });
     
     it("get the id of the Engineer", () => {
   
-        const result = new Engineer ("Thom Williams", 7, "thomwilliams1990@gmail.com", 436347);
+      const result = new Engineer ("Thom Williams", 7, "thomwilliams1990@gmail.com", "GitHub Profile");
   
       expect(result.getId()).toBe(7);
     });
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -5,28 +5,28 @@ describe("Manager", () => {
   
     it("assign the role of Manager", () => {
       
-        const result = new Manager ("Thom Williams", 7, "thomwilliams1990@gmail.com", "Manchester");
+      const result = new Manager ("Thom Williams", 7, "thomwilliams1990@gmail.com", 88888);
     
           expect(result.getRole()).toBe("Manager");
     });
 
     it("get the email address of the Manager", () => {
   
-        const result = new Manager ("Thom Williams", 7, "thomwilliams1990@gmail.com", "Manchester");
+      const result = new Manager ("Thom Williams", 7, "thomwilliams1990@gmail.com", 88888);
   
       expect(result.getEmail()).toBe("thomwilliams1990@gmail.com");
     });
 
     it("get the name of the Manager", () => {
   
-        const result = new Manager ("Thom Williams", 7, "thomwilliams1990@gmail.com", "Manchester");
+      const result = new Manager ("Thom Williams", 7, "thomwilliams1990@gmail.com", 88888);
   
       expect(result.getName()).toBe("Thom Williams");
     });
     
     it("get the id of the Manager", () => {
   
-        const result = new Manager ("Thom Williams", 7, "thomwilliams1990@gmail.com", "Manchester");
+      const result = new Manager ("Thom Williams", 7, "thomwilliams1990@gmail.com", 88888);
   
       expect(result.getId()).toBe(7);
     });
